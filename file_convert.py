14bd61d9beb67c58bb28e5cf209d467642b476cc
jeyachandranj <125851197+jeyachandranj@users.noreply.github.com>
2023-12-23 16:00:03 +0530

    Update file_convert.py

--- file_convert.py
+++ file_convert.py
@@ -15,6 +15,39 @@ class FileProcessor:
         with open(pickle_file, 'wb') as pkl_file:
             pickle.dump(self.data, pkl_file)
 
+    @staticmethod
+    def detect_file_type(file_name):
+        if '.csv' in file_name:
+            return 'csv'
+        elif '.json' in file_name:
+            return 'json'
+        elif '.xml' in file_name:
+            return 'xml'
+        elif '.txt' in file_name:
+            return 'txt'
+        elif '.bin' in file_name:
+            return 'bin'
+        else:
+            raise ValueError("Unsupported file format")
+
+    @staticmethod
+    def process_file(file_type, file_name):
+        processors = {
+            'csv': CSVProcessor,
+            'json': JSONProcessor,
+            'xml': XMLProcessor,
+            'txt': TXTProcessor,
+            'bin': BinaryProcessor,
+        }
+
+        processor_class = processors.get(file_type)
+        if processor_class:
+            processor = processor_class(file_name)
+            processor.read()
+            return processor
+        else:
+            raise ValueError("Unsupported file type")            
+
 class CSVProcessor(FileProcessor):
     def read(self):
         data = []
@@ -43,21 +76,34 @@ class XMLProcessor(FileProcessor):
             data.append(item_data)
         self.data = data
 
-file_name = 'sales.csv'
+class TXTProcessor(FileProcessor):
+    def read(self):
+        with open(self.file_name, 'r') as txt_file:
+            data = txt_file.read()
+            self.data = {'text_data': data}
 
-pickle_file = 'data.pickle'
+class BinaryProcessor(FileProcessor):
+    def read(self):
+        with open(self.file_name, 'rb') as bin_file:
+            data = bin_file.read()
+            self.data = {'binary_data': data}        
 
-if file_name.endswith('.csv'):
-    processor = CSVProcessor(file_name)
-elif file_name.endswith('.json'):
-    processor = JSONProcessor(file_name)
-elif file_name.endswith('.xml'):
-    processor = XMLProcessor(file_name)
-else:
-    raise ValueError("Unsupported file format")
 
-processor.read()
+def get_file_input():
+    file_path = input("Enter the file path: ")
+    return file_path
 
-processor.save_to_pickle(pickle_file)
+def main():
+    file_path = get_file_input()
 
-print(f"Data from '{file_name}' has been stored in '{pickle_file}' as a pickle file.")
+    try:
+        file_type = FileProcessor.detect_file_type(file_path)
+        processor = FileProcessor.process_file(file_type, file_path)
+        pickle_file = 'data.pickle'
+        processor.save_to_pickle(pickle_file)
+        print(f"Data from '{file_path}' has been stored in '{pickle_file}' as a pickle file.")
+    except ValueError as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
